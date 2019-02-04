8012e8c1e993187c411ed06c39645dc62cad2c8c
Mohamad Radwan <m.atef.radwan@gmail.com>
2019-02-04 08:58:23 +0000

    Sample

--- abnormal/data/models.py
+++ abnormal/data/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from taggit.managers import TaggableManager
 # Create your models here.
 class Data(models.Model):
     mrn = models.IntegerField('MRN')
     disc = models.CharField('Disc' , max_length = 200, default='None')
     text = models.TextField('text')
+    tags = TaggableManager()
+    
 
     def __str__(self):
         return self.disc
