ad5f36f35247adeadc60118210ff88ce4b8d5262
Mohamad Radwan <m.atef.radwan@gmail.com>
2019-02-04 02:54:13 +0000

    some mods

--- abnormal/data/urls.py
+++ abnormal/data/urls.py
@@ -5,6 +5,11 @@ from . import views
 
 urlpatterns = [
 
-    path ('' , views.index , name = 'index')
+    path ('' , views.index , name = 'index'), 
+    path ('<int:x>/', views.sample , name = 'detail'), 
+    path ('add/' , views.addData, name  = 'addData'),
+    path ('edit/<int:id>/' , views.editData, name  = 'editData')
+
+
 
 ]
--- abnormal/data/views.py
+++ abnormal/data/views.py
@@ -1,10 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Data
+from django.template import loader
+from .forms import dataForm
 # Create your views here.
 from django.http import HttpResponse
 # Create your tests here.
 def index(req):
-    return HttpResponse('Some data will be posted here')
 
+    dataList = Data.objects.order_by('id').reverse()
+    template = loader.get_template('data/index.html')
+    context = {
+        'dataList' : dataList
+    }
+    return render(req, 'data/index.html' , context)
+   # return HttpResponse(template.render(context, req))
 def sample (req, x):
     return HttpResponse('another sampke is here %s' %x)
+
+def addData (req):
+    if req.method == 'POST':
+        form  = dataForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form  = dataForm()
+        context = {
+        'form' :  form
+        }
+    return  render(req, 'data/addData.html', context)
+
+
+
+
+def editData (req, id):
+    if req.method =='GET':
+        form   = Data.objects.get(pk = id)
+        context ={
+
+            'data' : form 
+        }
+        return render(req, 'data/editData.html' , context)
+
+
+    
